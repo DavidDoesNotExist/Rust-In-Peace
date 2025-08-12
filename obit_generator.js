679cadb5a254c029725ca65240cac402171fbe0b
David Knoxxx <shieldmyskates@gmail.com>
2025-08-12 23:54:28 +0500

    Create obit_generator.js

---
+++ obit_generator.js
@@ -0,0 +1,33 @@
+import * as wasm from './obit_generator_bg.wasm';
+
+let wasm;
+
+const heap = new Array(32).fill(undefined);
+heap.push(undefined, null, true, false);
+
+function getObject(idx) { return heap[idx]; }
+
+let heap_next = heap.length;
+
+function addHeapObject(obj) {
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
+    heap[idx] = obj;
+    return idx;
+}
+
+function dropObject(idx) {
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+
+wasm = wasm_bindgen;
+Object.assign(module.exports, wasm);
